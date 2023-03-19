432c213053b12e1423abb18966198f2184d6f022
rohanaj2001 <rohanaj2001@gmail.com>
2023-03-19 17:55:38 +0530

    changes

--- functions/api.js
+++ functions/api.js
@@ -16,13 +16,13 @@ router.get('/', (req, res)=>{
   res.status(200).send({message : "success"});
 })
 
-router.use('/api/v1/admin', (req, res) => {
+router.use('/admin', (req, res) => {
   const targetUrl = process.env.TARGET_URL;
   
   // Proxy the request to the target server
   proxy.web(req, res, { target: targetUrl });
 });
 
-app.use('/.netlify/functions/api', router)
+app.use('/api/v1', router)
 
 module.exports.handler =   serverless(app);
