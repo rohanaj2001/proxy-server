c5741b4b6d1d219ebabb0389c0fba8ae097e559f
rohanaj2001 <rohanaj2001@gmail.com>
2023-03-19 06:21:22 +0530

    updated

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ const morgan = require('morgan');
 app.use(morgan('tiny'));
 
 app.get('/', (req, res)=>{
-  console.log("requested");
   res.status(200).send({message : "success"});
 })
 
-// app.use('/api/v1/admin', (req, res) => {
-//   const targetUrl = process.env.TARGET_URL;
+app.use('/api/v1/admin', (req, res) => {
+  const targetUrl = process.env.TARGET_URL;
   
-//   // Proxy the request to the target server
-//   proxy.web(req, res, { target: targetUrl });
-// });
+  // Proxy the request to the target server
+  proxy.web(req, res, { target: targetUrl });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Proxy server listening on port ${process.env.PORT}`);
