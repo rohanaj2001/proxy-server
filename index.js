e6a22524baae1c4cd4a88c4327a48e90fd5bee37
rohanaj2001 <rohanaj2001@gmail.com>
2023-03-19 05:58:49 +0530

    trying to fix bugs

--- index.js
+++ index.js
@@ -9,15 +9,16 @@ const morgan = require('morgan');
 app.use(morgan('tiny'));
 
 app.get('/', (req, res)=>{
+  console.log("requested");
   res.status(200).send({message : "success"});
 })
 
-app.use('/api/v1/admin', (req, res) => {
-  const targetUrl = process.env.TARGET_URL;
+// app.use('/api/v1/admin', (req, res) => {
+//   const targetUrl = process.env.TARGET_URL;
   
-  // Proxy the request to the target server
-  proxy.web(req, res, { target: targetUrl });
-});
+//   // Proxy the request to the target server
+//   proxy.web(req, res, { target: targetUrl });
+// });
 
 app.listen(8080, () => {
   console.log('Proxy server listening on port 8080');
