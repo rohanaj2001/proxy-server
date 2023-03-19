8474fec4f112266866356849407d09450aa2160a
rohanaj2001 <rohanaj2001@gmail.com>
2023-03-19 17:53:02 +0530

    changes

--- functions/api.js
+++ functions/api.js
@@ -2,15 +2,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
-const httpProxy = require('http-proxy');
+const serverless = require('serverless-http')
 const app = express();
 const router = express.Router()
+
+const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 const morgan = require('morgan');
 app.use(morgan('tiny'));
 
 
-const serverless = require('serverless-http')
 router.get('/', (req, res)=>{
   res.status(200).send({message : "success"});
 })
@@ -18,11 +19,10 @@ router.get('/', (req, res)=>{
 router.use('/api/v1/admin', (req, res) => {
   const targetUrl = process.env.TARGET_URL;
   
-app.use('/.netlify/functions/api', router)
   // Proxy the request to the target server
   proxy.web(req, res, { target: targetUrl });
 });
 
-
+app.use('/.netlify/functions/api', router)
 
 module.exports.handler =   serverless(app);
